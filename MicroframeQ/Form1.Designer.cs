b6cce91ce88d83bd156629e42eed397fa437ea4e
judeurban <73368004+judeurban@users.noreply.github.com>
2021-06-25 09:44:56 -0500

    Add project files.

---
+++ MicroframeQ/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace MicroframeQ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.truckListBox = new System.Windows.Forms.ListBox();
+            this.towerBox1 = new System.Windows.Forms.ListBox();
+            this.towerBox2 = new System.Windows.Forms.ListBox();
+            this.moveTower1 = new System.Windows.Forms.Button();
+            this.moveTower2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // truckListBox
+            // 
+            this.truckListBox.FormattingEnabled = true;
+            this.truckListBox.Location = new System.Drawing.Point(334, 79);
+            this.truckListBox.Name = "truckListBox";
+            this.truckListBox.Size = new System.Drawing.Size(131, 277);
+            this.truckListBox.TabIndex = 0;
+            // 
+            // towerBox1
+            // 
+            this.towerBox1.FormattingEnabled = true;
+            this.towerBox1.Location = new System.Drawing.Point(60, 79);
+            this.towerBox1.Name = "towerBox1";
+            this.towerBox1.Size = new System.Drawing.Size(131, 277);
+            this.towerBox1.TabIndex = 1;
+            // 
+            // towerBox2
+            // 
+            this.towerBox2.FormattingEnabled = true;
+            this.towerBox2.Location = new System.Drawing.Point(605, 79);
+            this.towerBox2.Name = "towerBox2";
+            this.towerBox2.Size = new System.Drawing.Size(131, 277);
+            this.towerBox2.TabIndex = 2;
+            // 
+            // moveTower1
+            // 
+            this.moveTower1.Location = new System.Drawing.Point(217, 212);
+            this.moveTower1.Name = "moveTower1";
+            this.moveTower1.Size = new System.Drawing.Size(75, 23);
+            this.moveTower1.TabIndex = 3;
+            this.moveTower1.Text = "Tower 1 <--";
+            this.moveTower1.UseVisualStyleBackColor = true;
+            this.moveTower1.Click += new System.EventHandler(this.MoveTower1_Click);
+            // 
+            // moveTower2
+            // 
+            this.moveTower2.Location = new System.Drawing.Point(504, 212);
+            this.moveTower2.Name = "moveTower2";
+            this.moveTower2.Size = new System.Drawing.Size(75, 23);
+            this.moveTower2.TabIndex = 4;
+            this.moveTower2.Text = "--> Tower 2";
+            this.moveTower2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.moveTower2);
+            this.Controls.Add(this.moveTower1);
+            this.Controls.Add(this.towerBox2);
+            this.Controls.Add(this.towerBox1);
+            this.Controls.Add(this.truckListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox truckListBox;
+        private System.Windows.Forms.ListBox towerBox1;
+        private System.Windows.Forms.ListBox towerBox2;
+        private System.Windows.Forms.Button moveTower1;
+        private System.Windows.Forms.Button moveTower2;
+    }
+}
+
---
+++ MicroframeQ/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MicroframeQ
+{
+    public partial class Form1 : Form
+    {
+        public static List<int> GlobalTruckCatalog = new List<int> { };
+        public static List<int> Tower1List = new List<int> { };
+        public static List<int> Tower2List = new List<int> { };
+        public static List<int> TempTruckList = new List<int> { };
+        BindingSource Tower1BS = new BindingSource();
+        BindingSource Tower2BS = new BindingSource();
+        BindingSource TempTruckListBS = new BindingSource();
+
+        public static string SaveFolder;
+        public static string UserSettingsPath;
+        public static string TruckListPath;
+        public Form1()
+        {
+            InitializeComponent();
+            Tower1BS.DataSource = Tower1List;
+            Tower2BS.DataSource = Tower2List;
+            TempTruckListBS.DataSource = TempTruckList;
+
+            try
+            {
+                SaveFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\MicroframeQ";
+                UserSettingsPath = SaveFolder + "\\UserSettings.txt";
+                TruckListPath = SaveFolder + "\\trucklist.txt";
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+            GlobalTruckCatalog.Clear();
+
+            //Read User Setings
+            ReadUserSettings();
+            TruckListBoxLoad();
+        }
+        private void TruckListBoxLoad()
+        {
+            //add all items of .txt file to catalog
+            string[] import_string = File.ReadAllLines(TruckListPath);
+
+            for (int i = 0; i < (int)import_string.Length; i++)
+            {
+                GlobalTruckCatalog.Add(int.Parse(import_string[i]));
+            }
+
+            GlobalTruckCatalog.Sort();
+
+            //set data source of listbox to global truck catalog
+            truckListBox.DataSource = GlobalTruckCatalog;
+
+            //copy the globaltruckcatalog into the temp list
+            foreach(int i in GlobalTruckCatalog)
+            {
+                TempTruckList.Add(i);
+            }
+        }
+        private void ReadUserSettings()
+        {
+            Console.WriteLine(SaveFolder);
+            if (!Directory.Exists(SaveFolder)) CreateUserSetings(SaveFolder);                            //make sure the directory actually exists before we read it 
+        }
+
+        private void CreateUserSetings(string SaveFolder)
+        {
+            //Try to create the directory at SaveFolder
+
+            DirectoryInfo di = Directory.CreateDirectory(SaveFolder);
+            Console.WriteLine("The directory was created successfully at {0}.", Directory.GetCreationTime(SaveFolder));
+
+            var myTruckFile = File.CreateText(TruckListPath);
+            var myUserSettingsFile = File.CreateText(UserSettingsPath);
+            myTruckFile.Close();
+            myUserSettingsFile.Close();
+
+            //Console.WriteLine("create trucklist file at " + trucklistpath);
+            //Console.WriteLine("create usersettings file at " + usersettingspath);
+
+            File.WriteAllText(TruckListPath, "");
+            File.WriteAllText(UserSettingsPath, "1\n1");
+
+            MessageBox.Show("Using " + SaveFolder + " data sucessfully stored");
+            MessageBox.Show("To change the serial COM port settings, use the dropdown menu above this message.");
+
+            myTruckFile.Close();
+            myUserSettingsFile.Close();
+            myUserSettingsFile.Close();
+
+        }
+        
+        private void MoveTower1_Click(object sender, EventArgs e)
+        {
+            if (truckListBox.Items.Count == 0)
+            {
+                return;
+            }
+            //if nothing is selected
+            if (truckListBox.SelectedItem == null)
+            {
+                MessageBox.Show("No item selected.");
+                return;
+            }
+            if (TempTruckList.Count == 0)
+            {
+                return;
+            }
+            //find selected item
+            //remove item from temptrucklist
+            //add item to tower1 listbox
+            //reset data source
+
+            int item_index;
+            string item_value;
+            //there's only one item, so we don't need to select a truck. Just remove the top item
+            if (truckListBox.Items.Count == 1)
+            {
+                item_index = 0;
+                item_value = (string)truckListBox.Items[0].ToString();
+            }
+            else
+            {
+                //define selected item to move
+                item_index = truckListBox.SelectedIndex;
+                item_value = truckListBox.SelectedItem.ToString();
+            }
+
+            //move from TempTruckList to Tower1List
+            TempTruckListBS.RemoveAt(item_index);
+            TempTruckList.Sort();
+            truckListBox.DataSource = TempTruckListBS;
+
+            Tower1BS.Add(Int32.Parse(item_value));
+            towerBox1.DataSource = Tower1BS;
+            //truckListBox.DataSource = TempTruckList;
+            //towerBox1.DataSource = Tower1List;
+
+        }
+    }
+}
